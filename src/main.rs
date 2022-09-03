510b1f0e7b3d7208a9c69c0ff0d133ea82218ef6
Adrian Berg <adrianrberg@protonmail.com>
2022-09-03 22:47:51 +0200

    It reads files and saves it.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-use native_dialog::{FileDialog};
-use std::process;
-use std::path::PathBuf;
-use std::io;
-use std::fs;
-use std::io::Cursor;
-use image::GenericImageView;
 use image::io::Reader as ImageReader;
+use image::{GenericImage, GenericImageView};
+use native_dialog::FileDialog;
+use std::fs;
+use std::io;
+use std::io::Cursor;
+use std::path::PathBuf;
+use std::process;
+use std::{thread, time};
+use simple_bar::ProgressBar;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
-
     let path = FileDialog::new()
         .set_location("~/Desktop")
         .add_filter("PNG Image", &["png"])
         .add_filter("JPEG Image", &["jpg", "jpeg"])
         .show_open_single_file()
         .unwrap();
-
+    println!("{:?}", path);
     match path {
         Some(path) => add_text_to_image(path),
         None => process::exit(1),
@@ -23,42 +25,77 @@ fn main() {
 }
 
 fn add_text_to_image(path: PathBuf) {
-    println!("Original binary:");
-    let text = get_text_input();
-    for el in &text {
-        print!("{:08b} ", el);
-        print!("         ");
-    };
-
-    let mut sliced_vector: Vec<u8> = Vec::new();
-    for byte in text {
+    let binary_vector = get_binary_vector();
+    let mut sliced_vector: Vec<u8> = Vec::with_capacity(binary_vector.len() * 2);
+    println!("{}", binary_vector.len());
+    let time1 = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_millis();
+    for byte in binary_vector {
+        // Get the 4 most significant bits
         sliced_vector.push(byte & 0xF0);
-        let mut x = byte & 0x0F;
-        // Shift 4 spots ahead so we can concern ourselves only with grabbing the most significant
-        // bits when we add text to our image.
-        x = x << 4;
+        // Shift to the left to get least significant bits in the most significant part
+        let x = byte << 4;
         sliced_vector.push(x);
     }
-    println!("Sliced binary:");
-    for el in &sliced_vector {
-        print!("{:08b} ", el);
-    };
-    // let img = ImageReader::open(path).unwrap().decode().unwrap();
-    //
-    // for x in img.pixels() {
-    //     println!("{:?}", x);
-    // }
+    let time2 = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_millis();
+    println!("Time taken: {}", time2 - time1);
+    println!("Starting to read image. ");
+    let img = ImageReader::open(path).unwrap().decode().unwrap();
+    println!("Read image into file, cloning...");
+    let mut new_img = img.clone();
+    println!("Cloned. \n");
+    let dimensions = img.dimensions();
+    if (dimensions.0 * dimensions.1) < sliced_vector.len() as u32 {
+        println!("Too much text for the given image.");
+        process::exit(1);
+    }
+    let mut should_break: bool = false;
+    println!("Starting to apply the vector to the image");
+    let mut bar = ProgressBar::default(sliced_vector.len() as u32, 50);
+    for mut pixel in img.pixels() {
+        if sliced_vector.len() < 3 {
+            println!("{:?}", sliced_vector);
+            sliced_vector.resize(3, 0);
+            println!("{:?}", sliced_vector);
+            should_break = true;
+        }
+
+        let bytes = (
+            sliced_vector[0] >> 4,
+            sliced_vector[1] >> 4,
+            sliced_vector[2] >> 4,
+        );
+        sliced_vector.remove(0);
+        sliced_vector.remove(0);
+        sliced_vector.remove(0);
+
+        let mut rgba = pixel.2; // Grabs the RGBA thingy
+        rgba[0] = (rgba[0] & 0xF0) + bytes.0;
+        rgba[1] = (rgba[1] & 0xF0) + bytes.1;
+        rgba[2] = (rgba[2] & 0xF0) + bytes.1;
+        new_img.put_pixel(pixel.0, pixel.1, rgba);
+        bar.next();
+        if should_break == true {
+            break;
+        }
+    }
+    new_img.save("finished.png").unwrap();
 }
 
-/// Get text input from the user, either by file or console.
-fn get_text_input() -> Vec<u8> {
+/// Get binary input from the user, either by file or text in console.
+fn get_binary_vector() -> Vec<u8> {
     println!("Choose between the following options:\n1) Add text by console input 2) Add text by text file");
     let mut choice = String::new();
     io::stdin()
         .read_line(&mut choice)
         .expect("Failed to read input. Exiting program");
     choice.pop();
-    let text = match choice.as_str() {
+    let mut bytes_vector = match choice.as_str() {
         // Clean and simple
         "1" => {
             println!("Type the text you want to embed into the image.\n");
@@ -66,8 +103,9 @@ fn get_text_input() -> Vec<u8> {
             io::stdin()
                 .read_line(&mut input)
                 .expect("Failed to read input. Exiting program");
-            input
-        },
+            let text_bytes = input.into_bytes();
+            text_bytes
+        }
         // Using a text file rather than console allows for more freeform, such as newlines and etc.
         // if the shell/terminal doesn't allow for that
         "2" => {
@@ -77,18 +115,16 @@ fn get_text_input() -> Vec<u8> {
                 .show_open_single_file()
                 .unwrap()
                 .expect("Not a valid path. Exiting program");
-            let input = fs::read_to_string(path)
-                .expect("Should have been able to read the file");
+            let input = fs::read(path).expect("Should have been able to read the file");
+            println!("Successfully read file into vector. Size: {}", input.len());
             input
-        },
+        }
         _ => {
             println!("Invalid option. Exiting program");
             process::exit(1);
         }
     };
 
-    let mut text_bytes = text.into_bytes();
-    text_bytes.push(0b00000000);
-    text_bytes
-
+    bytes_vector.push(0b00000000);
+    bytes_vector
 }
